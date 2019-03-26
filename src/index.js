15372dd748b168bce1058011f039085bc808e8ec
Sarp ISIK <sarp_isik91@hotmail.com>
2019-03-26 10:22:57 +0100

    Add loading indicator

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import { onNavigate } from './EventHandlers'
 import 'normalize.css'
 import './index.css'
 
-console.log(process.env.ANALYTICS_CREDENTIALS)
-
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!')
 }
 
 // Registering Our Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('/service-worker.js')
       .then(registration => {
@@ -19,29 +17,30 @@ if ('serviceWorker' in navigator) {
       .catch(registrationError => {
         console.log('SW registration failed: ', registrationError)
       })
-  })
-} else {
-  window.alert(
-    "Hi! Welcome to my portfolio web app. Looks like your browser doesn't support service worker. Which means you can visit it only online. But you can visit also offline by a service worker supported browser too. Have fun! :)"
-  )
-}
+  } else {
+    window.alert(
+      "Hi! Welcome to my portfolio web app. I just wanted to remind you that looks like your browser doesn't support service worker. Which means you can not visit me back offline. Have fun! :)"
+    )
+  }
 
-navigator.onLine ||
-  window.alert(
-    "'Looks like you are visiting my portfolio web app offline. Feel free to take a look and please visit back when you are online also, to catch some updates. Thanks!'"
-  )
+  navigator.onLine ||
+    window.alert(
+      'Looks like you are visiting my portfolio web app offline. Feel free to take a look and please visit back when you are online also, to catch some updates. Thanks!'
+    )
 
-// document.body.style.height = window.innerHeight + 'px';
+  // Portfolio box in intro
+  const portfolioLink = document.querySelector('.projects')
 
-const elementSelector = (name, id) =>
-  id ? document.getElementById(name) : document.querySelector(name)
+  // Left arrow navigator on nav bar in portfolio section
+  const leftArrow = document.querySelector('.arrow-left')
 
-// Portfolio box in intro
-const portfolioLink = elementSelector('.projects')
-
-// Left arrow navigator on nav bar in portfolio section
-const leftArrow = elementSelector('.arrow-left')
-
-window.addEventListener('load', () =>
+  // Click event on links
   onNavigate({ right: portfolioLink, left: leftArrow })
-)
+
+  const loading = document.querySelector('.intro-loader')
+  const main = document.getElementById('main')
+  setTimeout(() => {
+    loading.parentNode.removeChild(loading)
+    main.style = 'opacity: 1'
+  }, 1000)
+})
