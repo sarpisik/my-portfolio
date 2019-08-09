2df74e9cf9d80fdb981b4bcc5708d209a282f6d2
sarpisik <contact@sarpisik.com>
2019-08-09 09:19:58 +0300

    Add projects

---
+++ dist/precache-manifest.822049a1c70a2e8b773e2bcf419e03b1.js
@@ -0,0 +1,445 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb374ff454bbf9934461",
+    "url": "main.43b9afac7027c157b20c.css"
+  },
+  {
+    "revision": "cb374ff454bbf9934461",
+    "url": "main.7cde9fdd98e220466714.js"
+  },
+  {
+    "revision": "b31cb2a8e641f30eec49",
+    "url": "runtime.e4ce3e08ba37765e8a20.js"
+  },
+  {
+    "revision": "32b1b98e9ffc9a639cdf",
+    "url": "vendors.cef9bba19ef27b7110d9.css"
+  },
+  {
+    "revision": "32b1b98e9ffc9a639cdf",
+    "url": "vendors.49aa4cbe84faa43d1e14.js"
+  },
+  {
+    "url": "images/e8ad47571e34b971018eac48d9435cb9.png"
+  },
+  {
+    "url": "images/5bbc65a32743c5d1d4dbe8c8bc43c478.png"
+  },
+  {
+    "url": "images/bb5d30fec65686babae2e8caa53666c3.png"
+  },
+  {
+    "url": "images/ee4ad8588599c3413be5a42a6924ff97.png"
+  },
+  {
+    "url": "images/2a383627a4db098b28f0ec0adfc27e9d.jpg"
+  },
+  {
+    "url": "images/4875d42e69528e9cbadc7ba64aefac5f.png"
+  },
+  {
+    "url": "images/d2353148fa9cc3c2f973cb7b9098c391.png"
+  },
+  {
+    "url": "images/af80f2639d30ea5775994cc0d7df5c7c.jpg"
+  },
+  {
+    "url": "images/f3e15182143c6bbd594eb021a096b6d8.png"
+  },
+  {
+    "url": "images/262748545e389524992830dbc8671e7b.png"
+  },
+  {
+    "url": "images/59c467536111a981d904a050d335c2be.png"
+  },
+  {
+    "url": "fonts/681fa39a79c22f0035a0720e2b2bda3b.eot"
+  },
+  {
+    "url": "fonts/29f80b00a7b4641183f96f08be374697.ttf"
+  },
+  {
+    "url": "fonts/0f964a72f8fbdf9c8d4e0122b8effb40.woff"
+  },
+  {
+    "url": "images/74e6e6c3dcfca56767adabe83a510f2e.svg"
+  },
+  {
+    "url": "fonts/fbf7dfec8040aa387ce54ed0b206f434.svg"
+  },
+  {
+    "url": "fonts/eb54a705d8ad04a0ab6a79e5fb4ee4e8.woff"
+  },
+  {
+    "url": "fonts/af28bb8890fd35ab2c6f8c16ae16de03.woff2"
+  },
+  {
+    "url": "fonts/2479d9179eb53a1211fa8a6bb10b9f9b.eot"
+  },
+  {
+    "url": "fonts/dde09f55e9a05ead266ca2cfef7dacfc.ttf"
+  },
+  {
+    "url": "images/3c51cf95e0cf5b2145510b36882a66ed.svg"
+  },
+  {
+    "url": "fonts/cc5aa8a105931243a9c12221ded96383.svg"
+  },
+  {
+    "revision": "dd80d3e7ca0cace45e742d19d5f12584",
+    "url": "index.html"
+  },
+  {
+    "url": "manifest.4bbd92dc6aa7257e9a34d408590c5526.json"
+  },
+  {
+    "revision": "0678da41df533295333b3ccd10810d26",
+    "url": "favicon/android-chrome-512x512.png"
+  },
+  {
+    "revision": "2f7ba05531da5946c3e4dba2b1e0a8ae",
+    "url": "favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "a8f4acc0469aca7705a6829c987318bd",
+    "url": "favicon/browserconfig.xml"
+  },
+  {
+    "revision": "06a171a756aadf6fe811d301bd5103a3",
+    "url": "favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "c12949d6f623438a0f73a210ec262b71",
+    "url": "favicon/mstile-150x150.png"
+  },
+  {
+    "revision": "94e2e9c02cf9ce764037af82020c626f",
+    "url": "favicon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "8c1526fed83fee7ae434e813f7522d0c",
+    "url": "favicon/site.webmanifest"
+  },
+  {
+    "revision": "2a7fa2c9dd8b21b837c4db87982f5c6c",
+    "url": "favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "61a478fb69570a68e845ffcc9f4a690d",
+    "url": "favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "90f9f08561a4ae22a8232c73d8cf3f27",
+    "url": "favicon/favicon.ico"
+  },
+  {
+    "revision": "a989c8d3ef2f55541c43c309fb2e0121",
+    "url": "nasa/alert.3f5496d38ebada0a6ecb.js"
+  },
+  {
+    "revision": "0587db84d136c8e665420ace659b56c1",
+    "url": "nasa/alert.cbdf75f8c531cfbe2372.css"
+  },
+  {
+    "url": "nasa/fonts/2479d9179eb53a1211fa8a6bb10b9f9b.eot"
+  },
+  {
+    "url": "nasa/fonts/2e18ff3bf322185d4adf29ba5c5110d1.svg"
+  },
+  {
+    "url": "nasa/fonts/af28bb8890fd35ab2c6f8c16ae16de03.woff2"
+  },
+  {
+    "url": "nasa/fonts/dde09f55e9a05ead266ca2cfef7dacfc.ttf"
+  },
+  {
+    "url": "nasa/fonts/eb54a705d8ad04a0ab6a79e5fb4ee4e8.woff"
+  },
+  {
+    "revision": "fc71cfb081e90996047c245a55f833ce",
+    "url": "nasa/index.html"
+  },
+  {
+    "revision": "03cb7a295c790ec0ecc94a0a3b0162dd",
+    "url": "nasa/layout.67aef8f1732450f39f1c.js"
+  },
+  {
+    "revision": "4315bebbd0ccc6565a5823351be71571",
+    "url": "nasa/layout.c13c32706aa1e0177c2e.css"
+  },
+  {
+    "revision": "6633570a48454e67cd116fd09a1ab641",
+    "url": "nasa/main.4b3495cee5c80014086d.css"
+  },
+  {
+    "revision": "c41382f6497dff7657c644033fde283a",
+    "url": "nasa/main.7153a433b4fd67d79635.js"
+  },
+  {
+    "url": "nasa/manifest.4bbd92dc6aa7257e9a34d408590c5526.json"
+  },
+  {
+    "url": "nasa/precache-manifest.6375fed4f04a255e65f1f5bb0acf6187.js"
+  },
+  {
+    "revision": "e6f80f8ce929670358d209f67254e0a9",
+    "url": "nasa/runtime.34ba8866c6ef1edd009f.js"
+  },
+  {
+    "revision": "1bb4997745322158f100308b280c53c7",
+    "url": "nasa/search.df42b9b8ed3fccb0c976.js"
+  },
+  {
+    "revision": "97ab6b0271ed42728f9772c8090e7600",
+    "url": "nasa/service-worker.js"
+  },
+  {
+    "revision": "3b3ea0a830f6e5d4b47def2492659433",
+    "url": "rock-paper-scissors/game.9b349079cb6fc25cac8e.js"
+  },
+  {
+    "url": "rock-paper-scissors/images/155cc5f841fda739a4adc6b1a2d33518.png"
+  },
+  {
+    "url": "rock-paper-scissors/images/6d57f5faa02d24e27ebc1ff3a3d34a05.png"
+  },
+  {
+    "url": "rock-paper-scissors/images/f586e8f2084860252307318c189da2e4.png"
+  },
+  {
+    "revision": "c03c1bed51f6c494e24044269c2888fe",
+    "url": "rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "3461847cbeff6c5fa16b8e484f2e4a15",
+    "url": "rock-paper-scissors/main.44191f71d40d4d68eb85.css"
+  },
+  {
+    "revision": "6432d2668ab0c56943d8768ac1ad58f1",
+    "url": "rock-paper-scissors/main.7c4367d7b6fd8d114e55.js"
+  },
+  {
+    "url": "rock-paper-scissors/manifest.e246dc28aae7135d14f07982f29d41a8.json"
+  },
+  {
+    "url": "rock-paper-scissors/precache-manifest.7439bba2cf53ee8671570021544c9a90.js"
+  },
+  {
+    "revision": "aed2843095d1c5f3720249ec793b6e30",
+    "url": "rock-paper-scissors/runtime.99883a301de742b919e0.js"
+  },
+  {
+    "revision": "1870884691cb136343023c7e1ad7ffcd",
+    "url": "rock-paper-scissors/service-worker.js"
+  },
+  {
+    "revision": "1511ed6e131b9d0aac400901a474c2e3",
+    "url": "soccer-club/asset-manifest.json"
+  },
+  {
+    "revision": "02529490781d5b1689bc499b451cfe03",
+    "url": "soccer-club/favicon.ico"
+  },
+  {
+    "revision": "4c6629e5a79bae75231f632a9bf347e9",
+    "url": "soccer-club/index.html"
+  },
+  {
+    "revision": "5f564b09ee5b8af97aa82da12df74bad",
+    "url": "soccer-club/manifest.json"
+  },
+  {
+    "url": "soccer-club/precache-manifest.044f2fc3e875ec8047ef751aef9a80b1.js"
+  },
+  {
+    "url": "soccer-club/precache-manifest.4c58775568d1aa08e082e9aa26aeebb4.js"
+  },
+  {
+    "revision": "ef78651699df5bc906262bdf9cfedf81",
+    "url": "soccer-club/service-worker.js"
+  },
+  {
+    "revision": "b20000ba3036894434fa8675f9f8fa45",
+    "url": "soccer-club/static/js/2.ce49b665.chunk.js"
+  },
+  {
+    "revision": "6e52c64c3274107795f68e831b9d61a8",
+    "url": "soccer-club/static/js/3.1c89e4d3.chunk.js"
+  },
+  {
+    "revision": "957bb70287b036d56ac7bad92448cc35",
+    "url": "soccer-club/static/js/2.5e80c3b9.chunk.js"
+  },
+  {
+    "revision": "f9bd5c70aaad75407b5d21a70738aa31",
+    "url": "soccer-club/static/js/3.bc031ff9.chunk.js"
+  },
+  {
+    "revision": "48b083d7f4e57c89c25ef87b594c8ce0",
+    "url": "soccer-club/static/js/4.8e7383b5.chunk.js"
+  },
+  {
+    "revision": "2805986a9e2d1aa0ad7f85b6928dc303",
+    "url": "soccer-club/static/js/4.92b7b9d9.chunk.js"
+  },
+  {
+    "revision": "657514a78441621f6bffd98502d113e3",
+    "url": "soccer-club/static/js/5.154bc321.chunk.js"
+  },
+  {
+    "revision": "d41b63cf9c8eca92fa189083a58d6a68",
+    "url": "soccer-club/static/js/5.28033750.chunk.js"
+  },
+  {
+    "revision": "9045098ea773a72dc5d4ac09a2277c31",
+    "url": "soccer-club/static/js/6.17fc7183.chunk.js"
+  },
+  {
+    "revision": "21ffadef9193ba199ba93f0a21bbead7",
+    "url": "soccer-club/static/js/6.3850c2f3.chunk.js"
+  },
+  {
+    "revision": "50f8d83bd131807440d4d804b367ff90",
+    "url": "soccer-club/static/js/7.12811e6b.chunk.js"
+  },
+  {
+    "revision": "f99e7b49ce14a939bbba107f338090c3",
+    "url": "soccer-club/static/js/7.0bde0722.chunk.js"
+  },
+  {
+    "revision": "3140c6db3daae5331c3d76d5c0b4fe12",
+    "url": "soccer-club/static/js/7.7f047f4c.chunk.js"
+  },
+  {
+    "revision": "eb040f9436026bc0245d5d6b6b53e992",
+    "url": "soccer-club/static/css/main.60cbb3cb.chunk.css"
+  },
+  {
+    "revision": "96670a065591f61e4d0bad6639275f3e",
+    "url": "soccer-club/static/js/8.b6ddba31.chunk.js"
+  },
+  {
+    "revision": "da6ea0c16d104c1e11b0da1dca7498c6",
+    "url": "soccer-club/static/js/8.e3c45fd7.chunk.js"
+  },
+  {
+    "revision": "d3b5920aa2b7b914b1733aa834d498cc",
+    "url": "soccer-club/static/js/main.1c9ddb16.chunk.js"
+  },
+  {
+    "revision": "06878a5e077f290c9efa1da48dcf2a7c",
+    "url": "soccer-club/static/js/main.f04039cf.chunk.js"
+  },
+  {
+    "revision": "04fa6d48ba50e5cdded7d69de54f59ae",
+    "url": "soccer-club/static/js/main.f0840959.chunk.js"
+  },
+  {
+    "revision": "5dadaea5ae6f4422e79f4499b7e4c503",
+    "url": "soccer-club/static/js/runtime~main.146f75ba.js"
+  },
+  {
+    "revision": "2c93d3efd19815abb1cf43cfef85613f",
+    "url": "soccer-club/static/js/runtime~main.277a84ab.js"
+  },
+  {
+    "revision": "6d3d6c791ab4c33cc67b3c98381aa00e",
+    "url": "soccer-club/static/js/runtime~main.cbcb78da.js"
+  },
+  {
+    "revision": "178910ad6c132642f61dcae1bc6e4221",
+    "url": "soccer-club/static/media/1.178910ad.jpeg"
+  },
+  {
+    "revision": "d6f48180bce5a7f412ef0d324709e23a",
+    "url": "soccer-club/static/media/4.d6f48180.jpg"
+  },
+  {
+    "revision": "2878b5344eb45aeb2e2e41273ccaa2d4",
+    "url": "soccer-club/static/media/5.2878b534.jpg"
+  },
+  {
+    "revision": "b83d9f708d63939d6ea201033d86ce63",
+    "url": "soccer-club/static/media/d-1935811_640.b83d9f70.png"
+  },
+  {
+    "revision": "a4370619feeab62f71ab643dace34b1e",
+    "url": "soccer-club/static/media/design-751452_640.a4370619.png"
+  },
+  {
+    "revision": "7bd4dbdddf4b31e9469428dd3a8b6325",
+    "url": "soccer-club/static/media/business-779542_640.7bd4dbdd.jpg"
+  },
+  {
+    "revision": "dc76b779248730cb0cb8f01dc3a289ce",
+    "url": "soccer-club/static/media/lion-715852_640.dc76b779.png"
+  },
+  {
+    "revision": "a84d8e12f22421de1ea04f3641c5f238",
+    "url": "soccer-club/static/media/Muğlaspor.a84d8e12.png"
+  },
+  {
+    "revision": "bec1bf58f1443c077ef9b4e909326ac5",
+    "url": "soccer-club/static/media/fake_logo.bec1bf58.png"
+  },
+  {
+    "revision": "07bd2e9931da69d086a6a8583c65bb62",
+    "url": "soccer-club/static/media/7.07bd2e99.jpeg"
+  },
+  {
+    "revision": "a51fc52fc1cbfa7453a050d48db692da",
+    "url": "soccer-club/static/media/9.a51fc52f.jpeg"
+  },
+  {
+    "revision": "0e800529ab9d5cc071545875ed6e017a",
+    "url": "soccer-club/static/media/pexels-photo-1277213.0e800529.jpeg"
+  },
+  {
+    "revision": "1261b30be80803effb3bb54c7bbf431c",
+    "url": "soccer-club/static/media/pexels-photo-262524.1261b30b.jpeg"
+  },
+  {
+    "revision": "8e5f272284bdfa14316a9e9ff34d14df",
+    "url": "soccer-club/static/media/vector-2534950_640.8e5f2722.png"
+  },
+  {
+    "revision": "97a4ea7dfbb6c8e1af43d746602e437c",
+    "url": "soccer-club/static/media/pexels-photo-918798.97a4ea7d.jpeg"
+  },
+  {
+    "revision": "1f2ed44e0cce136f29ed5814ceff1ffc",
+    "url": "soccer-club/static/media/star-2822548_640.1f2ed44e.png"
+  },
+  {
+    "revision": "888939a7d1c2f1689350fc793ddab1eb",
+    "url": "soccer-club/static/media/8.888939a7.jpeg"
+  },
+  {
+    "revision": "0ed1891a7b556080c3877ab617a66c2a",
+    "url": "soccer-club/static/media/10.0ed1891a.jpeg"
+  },
+  {
+    "revision": "347a441d88bee406aecb065993f75dac",
+    "url": "soccer-club/static/media/3.347a441d.jpeg"
+  },
+  {
+    "revision": "f5d9a25b1a3ed7c5b9149aa38ee4a09a",
+    "url": "soccer-club/static/media/6.f5d9a25b.jpg"
+  },
+  {
+    "revision": "4135fb34c632af8b4f945f43df89a569",
+    "url": "soccer-club/static/media/pexels-photo-41257.4135fb34.jpeg"
+  },
+  {
+    "revision": "9d3948f6158ccd157a88eacd1ff0a735",
+    "url": "soccer-club/static/media/2.9d3948f6.jpeg"
+  },
+  {
+    "revision": "ced76caf8e102636139055e85a7e8825",
+    "url": "soccer-club/static/media/uniform1.ced76caf.png"
+  },
+  {
+    "revision": "97a371765aecd68ba3fb15dba317b4ce",
+    "url": "soccer-club/static/media/uniform2.97a37176.png"
+  }
+]);
---
+++ dist/rock-paper-scissors/precache-manifest.7439bba2cf53ee8671570021544c9a90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec04bca589b1b653c30",
+    "url": "/rock-paper-scissors/game.9b349079cb6fc25cac8e.js"
+  },
+  {
+    "url": "/rock-paper-scissors/images/155cc5f841fda739a4adc6b1a2d33518.png"
+  },
+  {
+    "url": "/rock-paper-scissors/images/6d57f5faa02d24e27ebc1ff3a3d34a05.png"
+  },
+  {
+    "url": "/rock-paper-scissors/images/f586e8f2084860252307318c189da2e4.png"
+  },
+  {
+    "revision": "c03c1bed51f6c494e24044269c2888fe",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "6fdd136d9adfdc02f103",
+    "url": "/rock-paper-scissors/main.44191f71d40d4d68eb85.css"
+  },
+  {
+    "revision": "6fdd136d9adfdc02f103",
+    "url": "/rock-paper-scissors/main.7c4367d7b6fd8d114e55.js"
+  },
+  {
+    "url": "/rock-paper-scissors/manifest.e246dc28aae7135d14f07982f29d41a8.json"
+  },
+  {
+    "revision": "468db789806f7e4d1c6e",
+    "url": "/rock-paper-scissors/runtime.99883a301de742b919e0.js"
+  }
+]);
