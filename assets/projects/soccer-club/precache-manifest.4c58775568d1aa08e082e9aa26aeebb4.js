cf5c3426685890217852aa7c35551ff93a9539ac
sarpisik <contact@sarpisik.com>
2019-09-02 18:14:07 +0300

    Add projects builds

---
+++ assets/projects/soccer-club/precache-manifest.4c58775568d1aa08e082e9aa26aeebb4.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d9a25b1a3ed7c5b9149aa38ee4a09a",
+    "url": "/soccer-club/static/media/6.f5d9a25b.jpg"
+  },
+  {
+    "revision": "2b13b5025f6db9637d4b",
+    "url": "/soccer-club/static/css/main.60cbb3cb.chunk.css"
+  },
+  {
+    "revision": "88f95c46ae1135d46b84",
+    "url": "/soccer-club/static/js/runtime~main.cbcb78da.js"
+  },
+  {
+    "revision": "3bc8c72195e1b4255e1c",
+    "url": "/soccer-club/static/js/2.ce49b665.chunk.js"
+  },
+  {
+    "revision": "a03f654afa9eb3543f97",
+    "url": "/soccer-club/static/js/3.1c89e4d3.chunk.js"
+  },
+  {
+    "revision": "26181abdcf29ddc7ae27",
+    "url": "/soccer-club/static/js/4.8e7383b5.chunk.js"
+  },
+  {
+    "revision": "df788885bcc0bef865c5",
+    "url": "/soccer-club/static/js/5.154bc321.chunk.js"
+  },
+  {
+    "revision": "0320d16f179b6228e131",
+    "url": "/soccer-club/static/js/6.17fc7183.chunk.js"
+  },
+  {
+    "revision": "3627fd992b36a54b5c27",
+    "url": "/soccer-club/static/js/7.7f047f4c.chunk.js"
+  },
+  {
+    "revision": "82d3c7a93a8a1ba077d6",
+    "url": "/soccer-club/static/js/8.b6ddba31.chunk.js"
+  },
+  {
+    "revision": "a84d8e12f22421de1ea04f3641c5f238",
+    "url": "/soccer-club/static/media/Muğlaspor.a84d8e12.png"
+  },
+  {
+    "revision": "178910ad6c132642f61dcae1bc6e4221",
+    "url": "/soccer-club/static/media/1.178910ad.jpeg"
+  },
+  {
+    "revision": "0ed1891a7b556080c3877ab617a66c2a",
+    "url": "/soccer-club/static/media/10.0ed1891a.jpeg"
+  },
+  {
+    "revision": "9d3948f6158ccd157a88eacd1ff0a735",
+    "url": "/soccer-club/static/media/2.9d3948f6.jpeg"
+  },
+  {
+    "revision": "347a441d88bee406aecb065993f75dac",
+    "url": "/soccer-club/static/media/3.347a441d.jpeg"
+  },
+  {
+    "revision": "d6f48180bce5a7f412ef0d324709e23a",
+    "url": "/soccer-club/static/media/4.d6f48180.jpg"
+  },
+  {
+    "revision": "2878b5344eb45aeb2e2e41273ccaa2d4",
+    "url": "/soccer-club/static/media/5.2878b534.jpg"
+  },
+  {
+    "revision": "2b13b5025f6db9637d4b",
+    "url": "/soccer-club/static/js/main.f04039cf.chunk.js"
+  },
+  {
+    "revision": "07bd2e9931da69d086a6a8583c65bb62",
+    "url": "/soccer-club/static/media/7.07bd2e99.jpeg"
+  },
+  {
+    "revision": "888939a7d1c2f1689350fc793ddab1eb",
+    "url": "/soccer-club/static/media/8.888939a7.jpeg"
+  },
+  {
+    "revision": "a51fc52fc1cbfa7453a050d48db692da",
+    "url": "/soccer-club/static/media/9.a51fc52f.jpeg"
+  },
+  {
+    "revision": "0e800529ab9d5cc071545875ed6e017a",
+    "url": "/soccer-club/static/media/pexels-photo-1277213.0e800529.jpeg"
+  },
+  {
+    "revision": "1261b30be80803effb3bb54c7bbf431c",
+    "url": "/soccer-club/static/media/pexels-photo-262524.1261b30b.jpeg"
+  },
+  {
+    "revision": "4135fb34c632af8b4f945f43df89a569",
+    "url": "/soccer-club/static/media/pexels-photo-41257.4135fb34.jpeg"
+  },
+  {
+    "revision": "97a4ea7dfbb6c8e1af43d746602e437c",
+    "url": "/soccer-club/static/media/pexels-photo-918798.97a4ea7d.jpeg"
+  },
+  {
+    "revision": "bec1bf58f1443c077ef9b4e909326ac5",
+    "url": "/soccer-club/static/media/fake_logo.bec1bf58.png"
+  },
+  {
+    "revision": "7bd4dbdddf4b31e9469428dd3a8b6325",
+    "url": "/soccer-club/static/media/business-779542_640.7bd4dbdd.jpg"
+  },
+  {
+    "revision": "b83d9f708d63939d6ea201033d86ce63",
+    "url": "/soccer-club/static/media/d-1935811_640.b83d9f70.png"
+  },
+  {
+    "revision": "a4370619feeab62f71ab643dace34b1e",
+    "url": "/soccer-club/static/media/design-751452_640.a4370619.png"
+  },
+  {
+    "revision": "dc76b779248730cb0cb8f01dc3a289ce",
+    "url": "/soccer-club/static/media/lion-715852_640.dc76b779.png"
+  },
+  {
+    "revision": "1f2ed44e0cce136f29ed5814ceff1ffc",
+    "url": "/soccer-club/static/media/star-2822548_640.1f2ed44e.png"
+  },
+  {
+    "revision": "8e5f272284bdfa14316a9e9ff34d14df",
+    "url": "/soccer-club/static/media/vector-2534950_640.8e5f2722.png"
+  },
+  {
+    "revision": "ced76caf8e102636139055e85a7e8825",
+    "url": "/soccer-club/static/media/uniform1.ced76caf.png"
+  },
+  {
+    "revision": "97a371765aecd68ba3fb15dba317b4ce",
+    "url": "/soccer-club/static/media/uniform2.97a37176.png"
+  },
+  {
+    "revision": "4c6629e5a79bae75231f632a9bf347e9",
+    "url": "/soccer-club/index.html"
+  }
+];
